0a3718c0add88d7d88792207d1f189074aa7f749
superSyd <37779021+superSyd@users.noreply.github.com>
2021-04-06 13:35:15 +1000

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -14,12 +14,12 @@ suite('Functional Tests', function() {
         .get("/api/convert?input=10L")
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.json, { 
-            initNum: 10, 
-            initUnit: 'L', 
-            returnNum: 2.64172, 
-            returnUnit: 'gal', 
-            string: '10 liters converts to 2.64172 gallons' });
+          assert.equal(res.body.initNum, 10);
+          assert.equal(res.body.initUnit, 'L');
+          assert.equal(res.body.returnNum, 2.64172);
+          assert.equal(res.body.returnUnit, 'gal');
+          assert.equal(res.body.string, '10 liters converts to 2.64172 gallons');
+
           done();
         });
     });
@@ -30,7 +30,7 @@ suite('Functional Tests', function() {
         .get("/api/convert?input=32g")
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.text, 'error')
+          assert.equal(res.text, 'invalid unit')
           done();
         });
     });
@@ -41,7 +41,7 @@ suite('Functional Tests', function() {
         .get("/api/convert?input=3/7.2/4kg")
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.text, 'error')
+          assert.equal(res.text, 'invalid number')
           done();
         });
     });
@@ -52,7 +52,7 @@ suite('Functional Tests', function() {
         .get("/api/convert?input=3/7.2/4kilomegagram")
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.text, 'error')
+          assert.equal(res.text, 'invalid number and unit')
           done();
         });
     });
@@ -63,12 +63,11 @@ suite('Functional Tests', function() {
         .get("/api/convert?input=kg")
         .end(function (err, res) {
           assert.equal(res.status, 200);
-          assert.equal(res.json, { 
-            initNum: 1, 
-            initUnit: 'kg', 
-            returnNum: 2.20462, 
-            returnUnit: 'lbs', 
-            string: '1 kilogram converts to 2.20462 pounds' })
+          assert.equal(res.body.initNum, 1);
+          assert.equal(res.body.initUnit, 'kg');
+          assert.equal(res.body.returnNum, 2.20462);
+          assert.equal(res.body.returnUnit, 'lbs');
+          assert.equal(res.body.string, '1 kilogram converts to 2.20462 pounds');
           done();
         });
     });
