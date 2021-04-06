16dc603f856daf5f0d14d9ca1d2d15c072d1446a
superSyd <37779021+superSyd@users.noreply.github.com>
2021-04-06 13:34:22 +1000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,11 +8,17 @@ module.exports = function (app) {
   let convertHandler = new ConvertHandler();
 
   app.route("/api/convert").get((req,res)=>{
-    var input = req.body.input;
+    console.log(req.url)
+    var input = req.query.input;
+    //console.log(req)
+    console.log(input)
     var initNum = convertHandler.getNum(input);
     var initUnit = convertHandler.getUnit(input);
+    console.log(initUnit)
+
     var returnUnit = convertHandler.getReturnUnit(initUnit);
     var returnNum = convertHandler.convert(initNum, initUnit);
+    
     var output = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
     return res.send(output);
